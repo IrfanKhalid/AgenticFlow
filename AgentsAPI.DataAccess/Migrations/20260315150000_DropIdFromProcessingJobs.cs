c0b868c4b78e81488ffa056560ea54a87c703741
Irfan-Khalid-max <mirfamkhalid@gmail.com>
2026-03-17 03:28:11 +0500

    droping column

--- AgentsAPI.DataAccess/Migrations/20260315150000_DropIdFromProcessingJobs.cs
+++ AgentsAPI.DataAccess/Migrations/20260315150000_DropIdFromProcessingJobs.cs
@@ -1,10 +1,14 @@
+using System;
+using AgentsAPI.DataAccess.Models;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace AgentsAPI.DataAccess.Migrations
 {
-    /// <inheritdoc />
+    [DbContext(typeof(AgentsDbContext))]
+    [Migration("20260315150000_DropIdFromProcessingJobs")]
     public partial class DropIdFromProcessingJobs : Migration
     {
         /// <inheritdoc />
